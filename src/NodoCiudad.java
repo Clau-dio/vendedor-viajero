c9430159e27ce23d4f000809c653b2ae3e30f556
Clau-dio <cha016@alumnos.ucn.cl>
2020-11-29 03:43:04 -0300

    Se implementó la clase Nodo Ciudad

--- src/NodoCiudad.java
+++ src/NodoCiudad.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 
 /**
  * @author Claudio__o
@@ -8,11 +5,66 @@
  */
 public class NodoCiudad {
 
+	/* Ciudad que guarda el nodo */
+	private Ciudad ciudad;
+	/* NodoCiudad siguiente */
+	private NodoCiudad next;
+	/* NodoCiudad anterior */
+	private NodoCiudad prev;
+	
 	/**
+	 * Función que se encarga que crear un nodo
 	 * 
+	 * @param ciudad : ciudad que contendra el nodo
 	 */
-	public NodoCiudad() {
-		// TODO Auto-generated constructor stub
+	public NodoCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+		this.next = null;
+	}
+	
+	/**
+	 *  Función que se encarga de devolver la ciudad que contiene el nodo actual
+	 * 
+	 * @return Ciudad
+	 */
+	public Ciudad getCiudad() {
+		return this.ciudad;
+	}
+	
+	/**
+	 * Función que se encagar de cambiar el nodo siguiente al nodo actual
+	 * 
+	 * @param next : Nuevo nodo anterior
+	 */
+	public void setNext(NodoCiudad next) {
+		this.next = next;
+	}
+	
+	/**
+	 * Función que se encagar de cambiar el nodo siguiente al nodo actual
+	 * 
+	 * @return NodoCiudad
+	 */
+	public NodoCiudad getNext() {
+		return this.next;
+	}
+	
+	/**
+	 * Función que se encagar de cambiar el nodo anterior al nodo actual
+	 * 
+	 * @param prev : Nuevo nodo anterior
+	 */
+	public void setPrev(NodoCiudad prev) {
+		this.prev = prev;
+	}
+	
+	/**
+	 * Función que se encarga de devolver el nodo anterior al nodo actual
+	 * 
+	 * @return NodoCiudad
+	 */
+	public NodoCiudad getPrev() {
+		return this.prev;
 	}
 
 }
